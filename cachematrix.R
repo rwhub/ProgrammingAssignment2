3d62b20cdfbc3379d0db0594443a392d7008b8b8
rwashburn <rex.washburn@gmail.com>
2014-07-27 17:34:39 -0500

    a few comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,21 @@
+# cachematrix.R: assignment 2 homework
+# 
+
+
 makeCacheMatrix <- function(x = matrix()) {
+    #instantiate inv
     inv <- NULL
+
     set <- function(y) {
         x <<- y
         inv <<- NULL
     }
+
     get <- function() x
     setinverse <- function(inverse) inv <<- inverse
     getinverse <- function() inv
+    
+    #return function list
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -14,16 +23,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 cacheSolve <- function(x, ...) {
-    inverse <- x$getinverse()
+    #leverage makeCacheMatrix()
+    i <- x$getinverse()
     
-    if(!is.null(inverse)) {
+    #if we have a cache get it and let the user know
+    if(!is.null(i)) {
         message("getting cached data.")
-        return(inverse)
+        return(i)
     }
+    #otherwise we get the data for solve()
     data <- x$get()
-    # matrix needs to be square for solve
-    inverse <- solve(data)
-    x$setinverse(inverse)
-    inverse
+    
+    # matrix needs to be square for solve()
+    i <- solve(data)
+    x$setinverse(i)
+
+    #return matrix
+    i
 }
 
